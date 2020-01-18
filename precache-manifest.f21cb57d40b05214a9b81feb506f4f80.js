ec3f6eaf97ef598e535535b69ec2859a9c170a91
Donald Fries <donald.darklight@gmail.com>
2020-01-18 11:15:55 -0600

    Updates

---
+++ precache-manifest.f21cb57d40b05214a9b81feb506f4f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b826b1fa8aa79cc5dc469f114b3501c5",
+    "url": "/fcc-wikipedia/index.html"
+  },
+  {
+    "revision": "21c278def9a421dc3748",
+    "url": "/fcc-wikipedia/static/css/main.288820e0.chunk.css"
+  },
+  {
+    "revision": "c435f5e5eb5c919bda57",
+    "url": "/fcc-wikipedia/static/js/2.17ee9868.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-wikipedia/static/js/2.17ee9868.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21c278def9a421dc3748",
+    "url": "/fcc-wikipedia/static/js/main.20600884.chunk.js"
+  },
+  {
+    "revision": "68c9faa04fe3d7233ade",
+    "url": "/fcc-wikipedia/static/js/runtime-main.8ac78e60.js"
+  }
+]);
